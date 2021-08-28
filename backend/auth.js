84affe3602493c27b99fdcf7093bf360d5e1e334
Jack Trimboli <67242835+JackTrimboli@users.noreply.github.com>
2021-08-27 20:55:47 -0400

    First commit

---
+++ backend/auth.js
@@ -0,0 +1,28 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+require("dotenv/config");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:5000/auth/google/callback",
+      passReqToCallback: true,
+    },
+    function (request, accessToken, refreshToken, profile, done) {
+      //   User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      // USE OTHER GOOGLE SERVICES HERE
+      return done(null, profile);
+      //   });
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
---
+++ backend/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+require("./auth");
+
+const app = express();
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => {
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/protected",
+    failureRedirect: "auth/failure",
+  })
+);
+
+app.get("/protected", isLoggedIn, (req, res) => {
+  res.send(`Hello ${req.user.displayName}`);
+});
+
+app.get("auth/failure", (req, res) => {
+  res.send("Authenication Failed.");
+});
+app.get("/logout", (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.send("Goodbye!");
+});
+app.listen(5000, () => console.log("Listening on PORT: 5000"));
